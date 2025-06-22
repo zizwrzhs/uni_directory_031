9a4d7ff80a44796951474a04ff7d70de68f99825
Zorz <georgopetrisg@gmail.com>
2025-06-22 17:19:18 +0300

    Created "Show Students"

--- conn_db.php
+++ conn_db.php
@@ -6,8 +6,8 @@
     $conn = new mysqli($server, $user, $password, $name);
 
     if ($conn->connect_error) {
-		die('Connection failed: '.$conn->connect_error);
-	} else {
-		mysqli_set_charset($conn, 'utf8');	
-	}
+    die('Connection failed: '.$conn->connect_error);
+    } else {
+    mysqli_set_charset($conn, 'utf8');	
+    }
 ?>
---
+++ results.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="G. Georgopetris - I. K. Soukeras">
+    <title>University Directory</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>No</th>
+        <?php
+            if (isset($_GET['type']) && $_GET['type'] == 'all') {
+                echo "<th>Type</th>";
+            }
+        ?>
+        <th>Student ID</th>
+        <th>Department ID</th>
+        <th>Professor ID</th>
+        <th>First Name</th>
+        <th>Middle Name</th>
+        <th>Last Name</th>
+        <th>Address</th>
+        <th>Phone Number</th>
+        <th>Email</th>
+        <th>Father Name</th>
+        <th>Gender</th>
+        <th>Birth Date</th>
+        <th>Birth Place</th>
+        <th>Citizenship</th>
+        <?php
+            if (isset($_GET['type']) && $_GET['type'] == 'under') {
+                echo "<th>Admission Rank</th>";
+                echo "<th>Admission Type</th>";
+            }
+            if (isset($_GET['type']) && $_GET['type'] == 'post') {
+                echo "<th>First Degree</th>";
+            }
+            if (isset($_GET['type']) && $_GET['type'] == 'ptc') {
+                echo "<th>University of Origin</th>";
+                echo "<th>Department of Origin</th>";
+            }
+        ?>
+    </tr>
+    <?php
+        include 'conn_db.php';
+
+        $DEPT_ID = $_GET['DEPT_ID'];
+        $type = $_GET['type'];
+
+        switch($type){
+            case 'all':
+                $sql = "SELECT s.*, CASE WHEN u.AM_ID IS NOT NULL THEN 'Προπτυχιακός' WHEN p.AM_ID IS NOT NULL THEN 'Μεταπτυχιακός' WHEN pt.AM_ID IS NOT NULL THEN 'PTC' ELSE 'unknown' END AS student_type FROM student s LEFT JOIN undergraduate u ON s.AM_ID = u.AM_ID LEFT JOIN postgraduate p ON s.AM_ID = p.AM_ID LEFT JOIN ptc pt ON s.AM_ID = pt.AM_ID WHERE DEPT_ID = '$DEPT_ID'";
+                break;
+            case 'under':
+                $sql = "SELECT * FROM student s, undergraduate u WHERE s.AM_ID = u.AM_ID AND DEPT_ID = '$DEPT_ID'";
+                break;
+            case 'post':
+                $sql = "SELECT * FROM student s, postgraduate p WHERE s.AM_ID = p.AM_ID AND DEPT_ID = '$DEPT_ID'";
+                break;
+            case 'ptc':
+                $sql = "SELECT * FROM student s, ptc WHERE s.AM_ID = ptc.AM_ID AND DEPT_ID = '$DEPT_ID'";
+                break;
+            default:
+                echo "Invalid type";
+                exit;
+        }
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $count = 0;
+            while($row = $result->fetch_assoc()) {
+                $count++;
+                if (isset($_GET['type']) && $_GET['type'] == 'all') {
+                    $stype = $row["student_type"];
+                }
+                $AM_ID = $row["AM_ID"];
+                $DEPT_ID = $row["DEPT_ID"];
+                $PROF_ID = $row["PROF_ID"];
+                $StudFname = $row["StudFname"];
+                $StudMname = $row["StudMname"];
+                $StudLname = $row["StudLname"];
+                $StudAddress = $row["StudAddress"];
+                $StudPhone = $row["StudPhoneNumber"];
+                $StudEmail = $row["StudEmail"];
+                $StudFatherName = $row["StudFatherName"];
+                $StudGender = $row["StudGender"];
+                $StudBirthDate = $row["StudBirthDate"];
+                $StudBirthPlace = $row["StudBirthPlace"];
+                $StudCitizenship = $row["StudCitizenship"];
+                if (isset($_GET['type']) && $_GET['type'] == 'under') {
+                    $AdmissionRank = $row["AdmissionRank"];
+                    $AdmissionType = $row["AdmissionType"];
+                }
+                if (isset($_GET['type']) && $_GET['type'] == 'post') {
+                    $FirstDegree = $row["FirstDegree"];
+                }
+                if (isset($_GET['type']) && $_GET['type'] == 'ptc') {
+                    $UniversityOfOrigin = $row["UniversityOfOrigin"];
+                    $DepartmentOfOrigin = $row["DepartmentOfOrigin"];
+                }
+
+                echo "<tr>";
+                echo "<td>$count</td>";
+                if (isset($_GET['type']) && $_GET['type'] == 'all') {
+                    echo "<td>$stype</td>";
+                }
+                echo "<td>$AM_ID</td>";
+                echo "<td>$DEPT_ID</td>";
+                echo "<td>$PROF_ID</td>";
+                echo "<td>$StudFname</td>";
+                echo "<td>$StudMname</td>";
+                echo "<td>$StudLname</td>";
+                echo "<td>$StudAddress</td>";
+                echo "<td>$StudPhone</td>";
+                echo "<td>$StudEmail</td>";
+                echo "<td>$StudFatherName</td>";
+                echo "<td>$StudGender</td>";
+                echo "<td>$StudBirthDate</td>";
+                echo "<td>$StudBirthPlace</td>";
+                echo "<td>$StudCitizenship</td>";
+                if (isset($_GET['type']) && $_GET['type'] == 'under') {
+                    echo "<td>$AdmissionRank</td>";
+                    echo "<td>$AdmissionType</td>";
+                }
+                if (isset($_GET['type']) && $_GET['type'] == 'post') {
+                    echo "<td>$FirstDegree</td>";
+                }
+                if (isset($_GET['type']) && $_GET['type'] == 'ptc') {
+                    echo "<td>$UniversityOfOrigin</td>";
+                    echo "<td>$DepartmentOfOrigin</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No results found";
+        }
+        $conn->close();
+    ?>
+</table>
+</body>
+</html>
